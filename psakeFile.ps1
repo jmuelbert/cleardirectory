c159b3f543382010c55f0d2727c21b91825da2b1
Jürgen Mülbert <jmuelbert@users.noreply.github.com>
2021-08-22 12:08:25 +0200

    Create psakeFile.ps1

---
+++ psakeFile.ps1
@@ -0,0 +1,9 @@
+properties {
+    if ($galleryApiKey) {
+        $PSBPreference.Publish.PSRepositoryApiKey = $galleryApiKey.GetNetworkCredential().password
+    }
+}
+
+task default -depends test
+
+task Test -FromModule PowerShellBuild -Version 0.6.1
