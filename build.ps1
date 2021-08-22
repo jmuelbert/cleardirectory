e7a51f9e87f4ba0e65f6708a37c6abc7757e7373
Jürgen Mülbert <jmuelbert@users.noreply.github.com>
2021-08-22 12:00:42 +0200

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,46 +1,43 @@
-<#
-.Description
-Installs and loads all the required modules for the build.
-Derived from scripts written by Warren F. (RamblingCookieMonster)
-#>
+[cmdletbinding(DefaultParameterSetName = 'Task')]
+param(
+    # Build task(s) to execute
+    [parameter(ParameterSetName = 'task', position = 0)]
+    [string[]]$Task = 'default',
 
-[cmdletbinding()]
-param ($Task = 'Default')
+    # Bootstrap dependencies
+    [switch]$Bootstrap,
 
-Write-Output "Starting build"
+    # List available build tasks
+    [parameter(ParameterSetName = 'Help')]
+    [switch]$Help,
 
-if (-not (Get-PackageProvider | ? Name -eq nuget))
-{
-    Write-Output "  Install Nuget PS package provider"
-    Install-PackageProvider -Name NuGet -Force -Confirm:$false | Out-Null
+    [pscredential]$PSGalleryApiKey
+)
+
+$ErrorActionPreference = 'Stop'
+
+# Bootstrap dependencies
+if ($Bootstrap.IsPresent) {
+    Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    if (-not (Get-Module -Name PSDepend -ListAvailable)) {
+        Install-Module -Name PSDepend -Repository PSGallery
+    }
+    Import-Module -Name PSDepend -Verbose:$false
+    Invoke-PSDepend -Path './requirements.psd1' -Install -Import -Force -WarningAction SilentlyContinue
 }
 
-$publishRepository = 'PSGallery'
-
-# Grab nuget bits, install modules, set build variables, start build.
-Write-Output "  Install And Import Build Modules"
-$psDependVersion = '0.1.62'
-if (-not(Get-InstalledModule PSDepend -RequiredVersion $psDependVersion -EA SilentlyContinue))
-{
-    Install-Module PSDepend -RequiredVersion $psDependVersion -Force -Scope CurrentUser
-}
-Import-Module PSDepend -RequiredVersion $psDependVersion
-Invoke-PSDepend -Path "$PSScriptRoot\build.depend.psd1" -Install -Import -Force
-
-if (-not (Get-Item env:\BH*)) 
-{
-    Set-BuildEnvironment
-    Set-Item env:\PublishRepository -Value $publishRepository
-}
-. "$PSScriptRoot\tests\Unload-SUT.ps1"
-
-Write-Output "  InvokeBuild"
-Invoke-Build $Task -Result result
-if ($Result.Error)
-{
-    exit 1
-}
-else 
-{
-    exit 0
+# Execute psake task(s)
+$psakeFile = './psakeFile.ps1'
+if ($Help.IsPresent) {
+    Get-PSakeScriptTasks -buildFile $psakeFile  |
+        Format-Table -Property Name, Description, Alias, DependsOn
+} else {
+    Set-BuildEnvironment -Force
+    $parameters = @{}
+    if ($PSGalleryApiKey) {
+        $parameters['galleryApiKey'] = $PSGalleryApiKey
+    }
+    Invoke-psake -buildFile $psakeFile -taskList $Task -nologo -parameters $parameters
+    exit ( [int]( -not $psake.build_success ) )
 }
