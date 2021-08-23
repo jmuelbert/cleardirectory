adac06ccf50f8a1da77af5417615c1bdc2c1f1f8
Jürgen <juergen.muelbert@gmail.com>
2021-08-23 11:19:36 +0200

    fix: build.yml

--- psakeFile.ps1
+++
@@ -1,9 +0,0 @@
-properties {
-    if ($galleryApiKey) {
-        $PSBPreference.Publish.PSRepositoryApiKey = $galleryApiKey.GetNetworkCredential().password
-    }
-}
-
-task default -depends test
-
-task Test -FromModule PowerShellBuild -Version 0.6.1
